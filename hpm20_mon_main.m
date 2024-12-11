195343147df6f485b1bb5ae77da07520b88d744f
Claire Treat <cctreat@gmail.com>
2024-12-11 15:03:12 +0100

    removed old parameterization file links at top of main model file

--- hpm20_mon_main.m
+++ hpm20_mon_main.m
@@ -29,18 +29,8 @@
 % READ IN PARAMETERS
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-% hpm20_mon_params_Toolik;
 % hpm20_mon_params_Lakkasuo;
-% hpm20_mon_params_Seida;
-% hpm20_mon_params_Ennadai_win;
-% hpm20_mon_params_Selwyn_win;
-%  hpm20_mon_params_JBL3_win
-% hpm20_mon_params_Ennadai_mac
-%  hpm20_mon_params_Ennadai_win
-% hpm20_mon_params_Ennadai_win_PFTchanges
- hpm20_mon_params_JoeyL_win
-% hpm20_mon_params_TKP_win;
-%  hpm20_mon_params_BailieBog_win;
+  hpm20_mon_params_JBL3_win
 params=load('hpm20_mon_param_vals');
 
 nveg = params.num_veg;
